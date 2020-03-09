457f0c5a0affc97502be01dcd89d562659fdf300
Rhema Makamba <rhemaexel7@gmail.com>
2020-03-09 16:02:29 +0200

    this is my new file

---
+++ task3.py
@@ -0,0 +1,10 @@
+import math
+a = 8
+b = 2
+c = 1
+d = 4
+print((math.sqrt(a-3))/((b**2)+(c**2)+(d**2)))
+
+from math import pi
+r = float(input("input the raduis of the circle"))
+print(" the area of the circle with raduis " + str(r) + " is: " + str(pi * r**2))
