504ac5c4049cb5fda7438596594b9c8a66484dbd
Rhema Makamba <rhemaexel7@gmail.com>
2020-03-10 16:09:32 +0200

    my commit

---
+++ MyPython.py
@@ -0,0 +1,23 @@
+
+StrSentence = "Lifechoice Code Acadamy"
+print(type(StrSentence))
+print(StrSentence[2]) #this cut the sencond letter#
+print(StrSentence[2:]) #this cuts out two letter of the sentence#
+Number1 = 25
+Number2 = 50.5
+Number3 = 3 + 7j
+print(type(Number1),type(Number2),type(Number3)) #this show the type of class#
+mylist = [65,25,125,78,120,33]
+print(type(mylist))
+mylist.sort()
+print(mylist)
+mylist.insert(2,37)
+print(mylist)
+Cars = ["Ford", "Benz", "BMW", "Shelby"]
+print(type(Cars))
+Cars.sort()
+print(Cars)
+mylist.reverse()
+print(mylist)
+xdict = {"1": "Fat Amy", "2":"Doggy", "3": "Tom"}
+print(type(xdict))
